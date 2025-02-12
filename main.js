1a320ce5f46a1d2fe053d41b5735a0f9e28973e7
Jinyehee <ounnms@gmail.com>
2025-02-12 17:27:42 +0900

    추가해야함

--- main.js
+++ main.js
@@ -4,7 +4,6 @@
 
 const todos = [];
 let i = 0;
-let checkChange = false;
 const todoAdd = $("#todoAdd");
 const todoUl = $("#todo_list");
 function todoList() {
@@ -34,6 +33,7 @@ function todoList() {
       content: span.text(),
       checked: check.is(":checked"),
       id: i,
+      checkChange: false,
     };
     todos.push(todo);
     console.log(todos);
@@ -46,7 +46,7 @@ function todoList() {
 }
 
 function makeTodo() {
-  todos.map((item) => {
+  todos.forEach((item) => {
     $(`#${item.id} #check`).click(() => {
       $(`#${item.id} span`).toggleClass("finsh");
       item.checked = $(`#${item.id} #check`).is(":checked");
@@ -61,25 +61,28 @@ function makeTodo() {
       $(`#${item.id}`).remove();
       console.log(todos);
     });
+
+    // 수정
+    $(`#${item.id} #change`).click(() => {
+      if (!item.checkChange) {
+        item.checkChange = true;
+        $(`#${item.id} span`).css("display", "none");
+        let todoChange = $("<input type='text'>").attr("id", "changeSpan");
+        todoChange.val($(`#${item.id} span`).text());
+        $(`#${item.id}`).append(todoChange);
+      } else {
+        let todoChange = $(`#${item.id} #changeSpan`);
+        if (todoChange.val()) {
+          item.checkChange = false;
+          $(`#${item.id} span`).text(todoChange.val());
+          $(`#${item.id} span`).css("display", "inline-block");
+          item.content = todoChange.val();
+          todoChange.remove();
+        } else {
+          alert("내용을 적어주세요...");
+        }
+      }
+      console.log(todos);
+    });
   });
 }
-
-// chgBtn.click(function () {
-//   if (!checkChange) {
-//     checkChange = true;
-//     span.css("display", "none");
-//     let todoChange = $("<input type='text'>").attr("id", "changeSpan");
-//     todoChange.val(span.text());
-//     li.append(todoChange);
-//   } else {
-//     let todoChange = $("#changeSpan");
-//     if (todoChange.val()) {
-//       checkChange = false;
-//       span.text(todoChange.val());
-//       span.css("display", "inline-block");
-//       todoChange.remove();
-//     } else {
-//       alert("내용을 적어주세요...");
-//     }
-//   }
-// });
