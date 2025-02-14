5f4b20cb1b6273c8f93f817d90a8ddcbac174f3f
Jinyehee <ounnms@gmail.com>
2025-02-14 16:15:17 +0900

    로컬스토리지 추가

--- main.js
+++ main.js
@@ -37,13 +37,21 @@ function thisDay() {
   );
 }
 
+// 날씨
 const api_key = "91bdbdffc330fa9fd5e5f1cf3e376ed4";
 const setWeather = $("#weather");
-navigator.geolocation.getCurrentPosition(function (position) {
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-
-  getWeather(lat, lon);
+setTimeout(() => {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      getWeather(lat, lon);
+    },
+    function (error) {
+      console.log("위치 정보 제공을 거부하였습니다.");
+      alert("위치 정보가 제공되지 않았습니다.");
+    }
+  );
 });
 
 function getWeather(lat, lon) {
@@ -53,7 +61,7 @@ function getWeather(lat, lon) {
     .then((response) => response.json())
     .then((data) => {
       const icon = data.weather[0].icon;
-      const iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+      const iconURL = `https://openweathermap.org/img/wn/${icon}@2x.png`;
       const temperature = Math.floor(data.main.temp);
       const description = data.weather[0].description;
       $("#icon").attr("src", iconURL);
@@ -64,10 +72,51 @@ function getWeather(lat, lon) {
     });
 }
 
+// 투두 추가
 const todos = [];
-let i = 0;
+
 const todoAdd = $("#todoAdd");
 const todoUl = $("#todo_list");
+
+// 엔터키
+$(document).on("keydown", function (event) {
+  if (event.key === "Enter" && todoAdd.val() !== "") {
+    todoList();
+  }
+});
+
+const saveTodoList = JSON.parse(localStorage.getItem("list"));
+console.log(saveTodoList);
+if (saveTodoList) {
+  for (let k = 0; k < saveTodoList.length; k++) {
+    todos.push(saveTodoList[k]);
+    console.log(todos);
+    let li = $("<li></li>").attr("id", saveTodoList[k].id);
+    let check = $("<input type='checkbox'>").attr("id", "check");
+    let span = $("<span></span>");
+    let delBtn = $("<button></button>").attr("id", "del");
+    let chgBtn = $("<button></button>").attr("id", "change");
+
+    li.append(check);
+    li.append(span);
+    li.append(delBtn);
+    li.append(chgBtn);
+
+    todoUl.append(li);
+
+    delBtn.text("삭제");
+    chgBtn.text("수정");
+
+    span.text(saveTodoList[k].content);
+
+    if (saveTodoList[k].checked === true) {
+      span.addClass("finsh");
+      check.prop("checked", true);
+    }
+    makeTodo();
+  }
+}
+let i = todos.length;
 function todoList() {
   if (todoAdd.val()) {
     let li = $("<li></li>").attr("id", i);
@@ -89,7 +138,6 @@ function todoList() {
     span.text(todoAdd.val());
 
     todoAdd.val("");
-
     // 객체에 넣은 후 배열에 넣는다
     const todo = {
       content: span.text(),
@@ -97,10 +145,10 @@ function todoList() {
       id: i,
       checkChange: false,
     };
+    i++;
     todos.push(todo);
     console.log(todos);
-    i++;
-
+    saveItem();
     makeTodo();
   } else {
     alert("내용을 입력해주세요...");
@@ -129,6 +177,7 @@ function makeTodo() {
             todoSpan.css("display", "inline-block");
             item.content = todoChange.val();
             todoChange.remove();
+            saveItem();
           } else {
             alert("내용을 적어주세요...");
           }
@@ -143,6 +192,7 @@ function makeTodo() {
         todoSpan.toggleClass("finsh");
         item.checked = $(`#${item.id} #check`).is(":checked");
         console.log(todos);
+        saveItem();
       });
 
     // 삭제
@@ -155,6 +205,12 @@ function makeTodo() {
         }
         $(`#${item.id}`).remove();
         console.log(todos);
+        saveItem();
       });
   });
+}
+
+// 로컬 스토리지
+function saveItem() {
+  localStorage.setItem("list", JSON.stringify(todos));
 }
